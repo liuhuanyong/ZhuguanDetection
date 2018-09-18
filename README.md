87342901126a131da2e8e1ba33c25f2bf279e98e
liu huanyong <lhy_in_blcu@126.com>
2018-09-18 15:08:12 +0800

    完成主观性计算接口

--- README.md
+++ README.md
@@ -4,14 +4,63 @@ Chinese Subjective Dectection based on subjective knowlegebase, 基于中文主�
 #项目介绍 
 主观性是中文舆情和文本挖掘一个必不可少的一项环节。
 对于英文而言，我调研过textblob的主观性接口，对于输入一个句子，textblob用的是词语主观性词典+加权的打法，相应的介绍可以查看我的总结材料(documents下的pdf文件)
-![image](https://github.com/liuhuanyong/ChineseSubjectiveDetection/blob/master/image/textblob_schema.png)
-
-对于中文而言，主观性判定往往和观点句识别混在一起，也将这个工作与情感分析中的观点句识别夹杂在一起。
-![image](https://github.com/liuhuanyong/ChineseSubjectiveDetection/blob/master/image/textblob_summary.png)
-
 而针对目前github开源平台上还缺少有对中文句子主观性评定的接口，本项目将尝试弥补这一空缺。
 本项目将从中文自身的特点出发，通过总结并挖掘中文句子主观性线索知识库和客观性线索知识库，提供一个面向中文句子的主观性判定方法。
 
-# 项目结构
+# 主观性字典
+1) degree_words.txt:程度副词，221个
+2) deny_words.txt:否定副词，29个
+3) lianci_words.txt:连词，93个
+4) nengyuan_words.txt:能愿副词，719个
+5) pingjia_words.txt:评价词，6846个
+6) qingtai_words.txt:情态动词，29个
+7) rencheng_words.txt:人称代词，32个
+8) senti_words:情感词，2090个
+9) tanci_words:叹词，139个
+10) yiwen_words:疑问代词，26个
+11) yuqi_words:语气词，17个
+12) zhishi_words.txt:指示代词，64个
+13) zhuangtai_words.txt:状态词，49个
+14) zhuzhang_words.txt:主张词，235个
 
-to be finished in next 2days..
+# 主观性计算规则
+1) 文本分句
+2) 计算每个句子的主观性。
+3) 每个句子主观性计算方式:主观线索词*主观线索词权重，做加权累加和平均
+3) 每个句子主观性求平均
+4) 输出文本主观性
+
+# 使用方式：
+    from zhuguang import *
+    handler = ZhuguanDetect()
+    sent = '''你要分析的文本'''
+    score = handler.detect(sent)
+    print(score)
+# 效果
+    content = '今天天气晴朗'
+    score = 0.0
+    **********************
+    content = '中华人民共和国万岁'
+    score = 0.0
+    **********************
+    content = '江龙船艇：台风“山竹”造成直接经济损失400万至500万'
+    score = 0.09375
+    **********************
+    content = '这两天经济不景气，恐怕这单子有得毁掉了'
+    score = 0.2092
+    **********************
+    content = '9月底美联储再次加息几乎板上钉钉，央行大概率小幅跟随加息，与此同时的定向降准对冲显得更为重要。预计10月前后可能再次实施定向降准措施。'
+    score = 0.237
+    **********************
+    content = '预计明后两天江西会有大到暴雨'
+    score = 0.36
+    **********************
+    content = '我喜欢你'
+    score = 0.767
+    *****************************
+
+# 总结
+1）本项目以词汇知识库的方式，通过总结归纳出主观性线索词，并加以规则进行计算。
+2) 从上面的效果来看，还像是那么回事
+3) 主观性配合情感得分，相信能够在舆情分析上起到一定作用
+4）词库欢迎补充。send mail to: lhy_in_blcu@126.com
---
+++ zhuguan.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# File: ZhuguanDetect.py
+# Author: lhy<lhy_in_blcu@126.com,https://huangyong.github.io>
+# Date: 18-9-6
+
+import jieba.posseg as pseg
+import os
+import re
+
+class ZhuguanDetect:
+    def __init__(self):
+        curdir = '/'.join(os.path.abspath(__file__).split('/')[:-1])
+        degree_file = os.path.join(curdir, 'dict/degree_words.txt')
+        deny_file = os.path.join(curdir, 'dict/deny_words.txt')
+        qingtai_file = os.path.join(curdir, 'dict/qingtai_words.txt')
+        zhuzhang_file = os.path.join(curdir, 'dict/zhuzhang_words.txt')
+        senti_file = os.path.join(curdir, 'dict/senti_words.txt')
+        pingjia_file = os.path.join(curdir, 'dict/pingjia_words.txt')
+        rencheng_file = os.path.join(curdir, 'dict/rencheng_words.txt')
+        zhishi_file = os.path.join(curdir, 'dict/rencheng_words.txt')
+        yiwen_file = os.path.join(curdir, 'dict/yiwen_words.txt')
+        lianci_file = os.path.join(curdir, 'dict/lianci_words.txt')
+        tanci_file = os.path.join(curdir, 'dict/tanci_words.txt')
+        yuqi_file = os.path.join(curdir, 'dict/yuqi_words.txt')
+        zhuangtai_file = os.path.join(curdir, 'dict/zhuangtai_words.txt')
+        nengyuan_file = os.path.join(curdir, 'dict/nengyuan_words.txt')
+
+        self.degree_words = self.load_words(degree_file)
+        self.deny_words = self.load_words(deny_file)
+        self.qingtai_words = self.load_words(qingtai_file)
+        self.zhuzhang_words = self.load_words(zhuzhang_file)
+        self.senti_words = self.load_words(senti_file)
+        self.pingjia_words = self.load_words(pingjia_file)
+        self.rencheng_words = self.load_words(rencheng_file)
+        self.zhishi_words = self.load_words(zhishi_file)
+        self.yiwen_words = self.load_words(yiwen_file)
+        self.lianci_words = self.load_words(lianci_file)
+        self.tanci_words = self.load_words(tanci_file)
+        self.yuqi_words = self.load_words(yuqi_file)
+        self.nengyuan_words = self.load_words(nengyuan_file)
+        self.zhuangtai_words = self.load_words(zhuangtai_file)
+
+    '''构造词典'''
+    def load_words(self, file):
+        return set([i.strip() for i in open(file) if i.strip()])
+
+    '''基于主观线索进行主观性分析'''
+    def zhuguan(self, sent):
+        scores = []
+        segs = [[w.word, w.flag] for w in pseg.cut(sent)]
+        for index, seg in enumerate(segs):
+            wd = seg[0]
+            postags = seg[1]
+            score = self.score_words(wd)
+            scores.append(score)
+        return sum(scores)/len(segs)
+
+    '''通过对主观性词语进行加权，用来计算'''
+    def score_words(self, word):
+        score = 0.0
+        if word in self.degree_words:
+            score = 0.75
+        elif word in self.deny_words:
+            score = 0.51
+        elif word in self.qingtai_words:
+            score = 0.81
+        elif word in self.rencheng_words:
+            score = 0.95
+        elif word in self.zhuzhang_words:
+            score = 0.98
+        elif word in self.senti_words:
+            score = 0.4
+        elif word in self.pingjia_words:
+            score = 0.95
+        elif word in self.zhishi_words:
+            score = 0.75
+        elif word in self.yiwen_words:
+            score = 0.9
+        elif word in self.lianci_words:
+            score = 0.88
+        elif word in self.tanci_words:
+            score = 0.75
+        elif word in self.yuqi_words:
+            score = 0.75
+        elif word in self.nengyuan_words:
+            score = 0.75
+        elif word in self.zhuangtai_words:
+            score = 0.6
+        return score
+
+    '''文章分句处理, 切分长句，冒号，分号，感叹号等做维护标识'''
+    def split_sents(self, content):
+        return [sentence for sentence in re.split(r'[？?！!。；;：:\n\r]', content) if sentence]
+
+    '''程序主函数'''
+    def detect(self, content):
+        sents = self.split_sents(content)
+        scores = []
+        for sent in sents:
+            sent_score = self.zhuguan(sent)
+            scores.append(sent_score)
+        return sum(scores)/len(scores)
+
+if __name__ == '__main__':
+    handler = ZhuguanDetect()
+    sent = '''江龙船艇：台风“山竹”造成直接经济损失400万至500万'''
+    score = handler.detect(sent)
+    print(score)
